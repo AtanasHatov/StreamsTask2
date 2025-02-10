d091b6bfee812aafada29e5d75565ae05f6b692a
AtanasHatov <atanashatov@gmail.com>
2025-02-10 11:04:06 +0200

    Add files via upload

---
+++ IPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public interface IPlayer
+    {
+        void Add(Player player);
+        bool Remove(Player player);
+        IEnumerable<Player> GetPlayers();
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public class Player
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (value == string.Empty)
+                {
+                    throw new ArgumentNullException("value");
+                }
+                this.name = value;
+            }
+        }
+
+        private string position;
+
+        public string Position
+        {
+            get { return position; }
+            set 
+            { 
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("value");
+                }
+                this.position = value;
+            }
+        }
+
+
+        public Player(string name, string position)
+        {
+            Name = name;
+            Position = position;
+        }
+    }
+}
